1080465867eb02803ed32a7b9385f1e937c18ccc
David-Lee-dev <aganga7427@gmail.com>
2023-09-12 09:15:58 +0900

    .

--- algorithm/py.make.sh
+++ algorithm/py.make.sh
@@ -4,11 +4,23 @@ read filename
 echo "Enter url"
 read url
 
+allowed_sources=("programmers" "swea")
+echo "Select a source (options: ${allowed_sources[*]}):"
+select source in "${allowed_sources[@]}"; do
+  if [[ " ${allowed_sources[*]} " == *" $source "* ]]; then
+    break
+  else
+    echo "Invalid source. Please select a valid source."
+  fi
+done
+
 cat << EOF > "$filename".py
 # $filename
 # $url
-
+# $source
 
 EOF
 
-mv "$filename.py" python/
+destination="python/$source/$filename"
+mkdir -p "$destination"
+mv "$filename.py" "$destination/$filename.py"
