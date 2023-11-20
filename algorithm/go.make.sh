5936c87145c79eea2cf8f240ff6f86fdea4c5cef
David-Lee-dev <aganga7427@gmail.com>
2023-11-20 18:23:11 +0900

    script

--- algorithm/go.make.sh
+++ algorithm/go.make.sh
@@ -1,14 +1,14 @@
 
-echo "Enter a filename for your new Markdown file (without the .go extension):"
+echo "Enter a filename for your new Markdown file (without the .md extension):"
 read filename
 echo "Enter url"
 read url
 
-cat << EOF > "$filename".go
+cat << EOF > "$filename".md
 // $filename
 // $url
 
 
 EOF
 
-mv "$filename.go" go/
+mv "$filename.md" go/
